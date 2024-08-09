6e755c647d4b4a6b6d12477b3f688cf8a92a10d9
AndrewNguyen <khanamdev@gmail.com>
2024-08-09 08:51:42 +0700

    update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@ $(function(){
     $(".showmenu-pc").click(function(){
         $(".menu-pc").addClass("animate__animated animate__fadeInDownBig show")
     });
+    $(".showmenu-mobile").click(function(){
+        $(".menu-pc").addClass("animate__animated animate__fadeInDownBig show")
+    });
     $(".closemenu-pc").click(function(){
         $(".menu-pc").removeClass("animate__animated animate__fadeInDownBig show")
     });
+
+    $(window).scroll(function (){
+        if($(window).scrollTop() > 1){
+            $("#header").addClass('bg-white')
+        } else {
+            $("#header").removeClass('bg-white')
+        }
+    })
 });
