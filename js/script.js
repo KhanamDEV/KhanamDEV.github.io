eb260372475da4a41388b95a9ba9e1d071ada068
AndrewNguyen <khanamdev@gmail.com>
2024-08-11 23:23:50 +0700

    update

--- js/script.js
+++ js/script.js
@@ -9,10 +9,11 @@ $(function(){
         $(".menu-pc").removeClass("animate__animated animate__fadeInDownBig show")
     });
     $(document).on('scroll', function() {
-        for(let i = 2; i<= 7; i++){
-            if( $(this).scrollTop() + window.screen.height * 1.5 >= $(`.effect-item.${i}`).position().top ){
+        for(let i = 2; i<= 9; i++){
+            if( $(this).scrollTop() + window.screen.height * 0.8 >= $(`.effect-item.${i}`).position().top ){
                 if(!$(`.effect-item.${i} img`).hasClass('animate__bounceInUp')){
                     $(`.effect-item.${i} img`).addClass('animate__bounceInUp')
+                    $(`.effect-item.${i} img`).removeClass('hide')
                 }
             }
         }
