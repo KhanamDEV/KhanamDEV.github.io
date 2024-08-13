ee785f6f0087f09eabc18102e849952cd91f92de
AndrewNguyen <khanamdev@gmail.com>
2024-08-13 10:29:23 +0700

    update

--- js/script.js
+++ js/script.js
@@ -11,50 +11,50 @@ $(function () {
 
     let previousHeight = 0;
     let max = 0
-    $(document).on('scroll', function () {
+    // $(document).on('scroll', function () {
         
-        if(previousHeight < $(this).scrollTop()){
-            for (let i = 1; i <= 9; i++) {
-                console.log(max, i, "bottom");
-                if ($(this).scrollTop() + window.screen.height * 0.8 >= $(`.effect-item.${i}`).position().top && i > max) {
-                    console.log(i);
-                    for (let j = 1; j <= 9; j++) {
-                        if (j < i) {
-                            $(`.effect-item.${j} img`).removeClass('animate__bounceInUp')
-                            $(`.effect-item.${j} img`).addClass('hide');
-                        }
-                    }
-                    if (!$(`.effect-item.${i} img`).hasClass('animate__bounceInUp')) {
-                        $(`.effect-item.${i} img`).addClass('animate__bounceInUp')
-                        $(`.effect-item.${i} img`).removeClass('hide')
-                    }
-                    max = i;
-                    break;
-                }
-            }
-        } else {
+        // if(previousHeight < $(this).scrollTop()){
+        //     for (let i = 1; i <= 9; i++) {
+        //         console.log(max, i, "bottom");
+        //         if ($(this).scrollTop() + window.screen.height * 0.8 >= $(`.effect-item.${i}`).position().top && i > max) {
+        //             console.log(i);
+        //             for (let j = 1; j <= 9; j++) {
+        //                 if (j < i) {
+        //                     $(`.effect-item.${j} img`).removeClass('animate__bounceInUp')
+        //                     $(`.effect-item.${j} img`).addClass('hide');
+        //                 }
+        //             }
+        //             if (!$(`.effect-item.${i} img`).hasClass('animate__bounceInUp')) {
+        //                 $(`.effect-item.${i} img`).addClass('animate__bounceInUp')
+        //                 $(`.effect-item.${i} img`).removeClass('hide')
+        //             }
+        //             max = i;
+        //             break;
+        //         }
+        //     }
+        // } else {
             
-            for (let i = 9; i >= 1; i--) {
-               if ($(this).scrollTop()  <= $(`.effect-item.${i}`).position().top + window.screen.height * 0.8 && i < max) {
-                    for (let j = 1; j <= 9; j++) {
-                        if (j > i) {
-                            $(`.effect-item.${j} img`).removeClass('animate__bounceInUp')
-                            $(`.effect-item.${j} img`).addClass('hide');
-                        }
-                    }
-                    if (!$(`.effect-item.${i} img`).hasClass('animate__bounceInUp')) {
-                        $(`.effect-item.${i} img`).addClass('animate__bounceInUp')
-                        $(`.effect-item.${i} img`).removeClass('hide')
-                    }
-                    max = i;
-                    break;
-                }
-            }
+        //     for (let i = 9; i >= 1; i--) {
+        //        if ($(this).scrollTop()  <= $(`.effect-item.${i}`).position().top + window.screen.height * 0.8 && i < max) {
+        //             for (let j = 1; j <= 9; j++) {
+        //                 if (j > i) {
+        //                     $(`.effect-item.${j} img`).removeClass('animate__bounceInUp')
+        //                     $(`.effect-item.${j} img`).addClass('hide');
+        //                 }
+        //             }
+        //             if (!$(`.effect-item.${i} img`).hasClass('animate__bounceInUp')) {
+        //                 $(`.effect-item.${i} img`).addClass('animate__bounceInUp')
+        //                 $(`.effect-item.${i} img`).removeClass('hide')
+        //             }
+        //             max = i;
+        //             break;
+        //         }
+        //     }
 
-        }
-        previousHeight = $(this).scrollTop();
+        // }
+        // previousHeight = $(this).scrollTop();
 
-    });
+    // });
 
     $(window).scroll(function () {
         if ($(window).scrollTop() > 1) {
