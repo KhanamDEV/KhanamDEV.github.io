b261254006e1184cfb03f715bdbb1ff5e8ffe72f
AndrewNguyen <khanamdev@gmail.com>
2024-09-06 10:58:59 +0700

    update

--- js/script.js
+++ js/script.js
@@ -19,59 +19,49 @@ $(function () {
     $(".openbtn").click(function () {
         $(".openbtn").toggleClass('active');
     });
-
-    let previousHeight = 0;
-    let max = 0
-    // $(document).on('scroll', function () {
-        
-        // if(previousHeight < $(this).scrollTop()){
-        //     for (let i = 1; i <= 9; i++) {
-        //         console.log(max, i, "bottom");
-        //         if ($(this).scrollTop() + window.screen.height * 0.8 >= $(`.effect-item.${i}`).position().top && i > max) {
-        //             console.log(i);
-        //             for (let j = 1; j <= 9; j++) {
-        //                 if (j < i) {
-        //                     $(`.effect-item.${j} img`).removeClass('animate__bounceInUp')
-        //                     $(`.effect-item.${j} img`).addClass('hide');
-        //                 }
-        //             }
-        //             if (!$(`.effect-item.${i} img`).hasClass('animate__bounceInUp')) {
-        //                 $(`.effect-item.${i} img`).addClass('animate__bounceInUp')
-        //                 $(`.effect-item.${i} img`).removeClass('hide')
-        //             }
-        //             max = i;
-        //             break;
-        //         }
-        //     }
-        // } else {
-            
-        //     for (let i = 9; i >= 1; i--) {
-        //        if ($(this).scrollTop()  <= $(`.effect-item.${i}`).position().top + window.screen.height * 0.8 && i < max) {
-        //             for (let j = 1; j <= 9; j++) {
-        //                 if (j > i) {
-        //                     $(`.effect-item.${j} img`).removeClass('animate__bounceInUp')
-        //                     $(`.effect-item.${j} img`).addClass('hide');
-        //                 }
-        //             }
-        //             if (!$(`.effect-item.${i} img`).hasClass('animate__bounceInUp')) {
-        //                 $(`.effect-item.${i} img`).addClass('animate__bounceInUp')
-        //                 $(`.effect-item.${i} img`).removeClass('hide')
-        //             }
-        //             max = i;
-        //             break;
-        //         }
-        //     }
-
-        // }
-        // previousHeight = $(this).scrollTop();
-
-    // });
-
     $(window).scroll(function () {
         if ($(window).scrollTop() > 1) {
             $("#header").addClass('bg-white')
         } else {
             $("#header").removeClass('bg-white')
         }
-    })
+    });
+
+
 });
+
+(function(){
+    function id(v){ return document.getElementById(v); }
+    function loadbar() {
+      var ovrl = id("overlay"),
+          prog = id("progress"),
+          stat = id("progstat"),
+          number = id("progNumber");
+        let step = 0;
+      const intervalLoading = setInterval(() => {
+        step += 10;
+        var perc =  step +"%";
+        prog.style.width = perc;
+        stat.innerHTML = "Loading..."  ;
+        number.innerHTML = step;
+        if(step == 100) return doneLoading();
+      }, 200)
+  
+     
+      function doneLoading(){
+        clearInterval(intervalLoading)
+        ovrl.style.opacity = 0;
+        setTimeout(function(){ 
+          ovrl.style.display = "none";
+
+          setTimeout(() => {
+            $("#banner .sub-title").addClass("animate__animated animate__bounceInUp")
+            $("#banner .sub-title").removeClass("d-none");
+            $("#banner h1").text("可能性の扉を解き放て")
+          }, 1500)
+        }, 1200);
+      }
+        
+    }
+    document.addEventListener('DOMContentLoaded', loadbar, false);
+  }());
