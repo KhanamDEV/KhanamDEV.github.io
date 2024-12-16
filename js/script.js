e336244620c70b36b9a13c9099d4663fffdb64c0
AndrewNguyen <khanamdev@gmail.com>
2024-12-16 07:50:06 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
 $(function () {
-
-  function toggleAllowPopup() {
-    function toggle(current) {
-      console.log(`Current value: ${current.value}`);
-      browser.browserSettings.allowPopupsForUserEvents.set({ value: !current.value });
-    }
-    browser.browserSettings.allowPopupsForUserEvents.get({}).then(toggle);
-  }
-
-  browser.browserAction.onClicked.addListener(() => {
-    toggleAllowPopup();
-  });
-
-
   localStorage.setItem('sources', JSON.stringify([]));
   $(".checkbox-print").click(function () {
     let source = localStorage.getItem('sources') == null ? [] : JSON.parse(localStorage.getItem('sources'));
