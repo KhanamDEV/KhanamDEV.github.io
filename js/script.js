4ec20a91ce356ec5166bfed9666a15fcb57a4d70
AndrewNguyen <khanamdev@gmail.com>
2024-12-16 10:55:42 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
 $(function () {
   localStorage.setItem('sources', JSON.stringify([]));
   $(".checkbox-print").click(function () {
-    Swal.fire({
-      title: "",
-      allowEscapeKey: false,
-      allowOutsideClick: false,
-      showConfirmButton: false,
-      willOpen(popup) {
-        Swal.showLoading();
-      },
-    });
+
     let source = localStorage.getItem('sources') == null ? [] : JSON.parse(localStorage.getItem('sources'));
     let src = $(this).parent().find('img').attr('src');
     if (source.includes(src)) {
@@ -18,9 +10,7 @@ $(function () {
       source.push(src);
     }
     localStorage.setItem('sources', JSON.stringify(source))
-    setTimeout(function () {
-      Swal.close()
-    }, 700)
+
   })
   $('.btn-print').click(() => {
     let source = localStorage.getItem('sources') == null ? [] : JSON.parse(localStorage.getItem('sources'));
